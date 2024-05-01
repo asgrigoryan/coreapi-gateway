f6a0ab21f86075e7ebd725f17108cc4033570aa2
astghikgrigoryanpcln <130073159+astghikgrigoryanpcln@users.noreply.github.com>
2024-05-02 02:55:19 +0400

    Draft implementation for the spring cloud gateway

---
+++ src/main/java/com/core/coreapigateway/CoreapiGatewayApplication.java
@@ -0,0 +1,13 @@
+package com.core.coreapigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoreapiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CoreapiGatewayApplication.class, args);
+	}
+
+}
