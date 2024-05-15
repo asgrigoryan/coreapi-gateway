e31692360811d42c7beab274b60f482c9f0b5f46
astghikgrigoryanpcln <130073159+astghikgrigoryanpcln@users.noreply.github.com>
2024-05-16 00:42:11 +0400

    fix

--- src/main/java/com/core/coreapigateway/filters/LoggingWithConfigGatewayFilterFactory.java
+++ src/main/java/com/core/coreapigateway/filters/LoggingWithConfigGatewayFilterFactory.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.OrderedGatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpStatusCode;
 import org.springframework.stereotype.Component;
 import reactor.core.publisher.Mono;
 
@@ -13,18 +14,29 @@ import reactor.core.publisher.Mono;
 @Component
 public class LoggingWithConfigGatewayFilterFactory extends AbstractGatewayFilterFactory<LoggingWithConfigGatewayFilterFactory.Config> {
 
+    public LoggingWithConfigGatewayFilterFactory() {
+        super(Config.class);
+    }
     @Override
     public GatewayFilter apply(Config config) {
         return new OrderedGatewayFilter(((exchange, chain) -> {
-            log.info("Request from the client {} accepted by gateway.", config.getName());
+            log.info("Request from the client {} accepted by gateway.", config.getParam());
             return chain.filter(exchange)
-                    .then(Mono.fromRunnable(()->log.info("Response from the service returned to the gateway.")));
+                    .doOnError((ex) -> log.error("Error processing request: {}", ex.getMessage()))
+                    .then(Mono.fromRunnable(() -> {
+                        HttpStatusCode responseStatus = exchange.getResponse().getStatusCode();
+                        if (responseStatus != null) {
+                            log.info("Response from the service returned to the gateway with status code: {}", responseStatus.value());
+                        } else {
+                            log.warn("Response status code could not be determined");
+                        }
+                    }));
         }), -1);
     }
 
     @Setter
     @Getter
-    public static class Config{
-        private String name;
+    public static class Config {
+        private String param;
     }
 }
